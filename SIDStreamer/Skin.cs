18abb2369e158afce408c4b09704799d8a416292
Rune Devik <runedevik@yahoo.no>
2025-12-25 21:39:14 +0100

    Remove fluff

--- SIDStreamer/Skin.cs
+++ SIDStreamer/Skin.cs
@@ -1,19 +1,14 @@
 ﻿namespace SIDStream
 {
+    // Class used to serialize and deserialize a specific skin's parameters from JSON
     internal class Skin
     {
-        // Transparent background image for player
         public string bgImage { get; set; }
-
-        // Default value of media label on startup
         public string mediaLabel { get; set; }
         public string mediaLabelBGColor { get; set; }
         public string mediaLabelFGColor { get; set; }
         public int mediaLabelX { get; set; }
         public int mediaLabelY { get; set; }
-
-
-        // Value of info label e.g. SIDstreamer v.1.0.
         public string infoLabel { get; set; }
         public string infoLabelBGColor { get; set; }
         public string infoLabelFGColor { get; set; }
@@ -22,31 +17,21 @@
         public string copyrightLabel { get; set; }
         public string copyrightLabelBGColor { get; set; }
         public string copyrightLabelFGColor { get; set; }
-
         public int copyrightLabelX { get; set; }
         public int copyrightLabelY { get; set; }
-
-
-        // Set position of volume slider
         public int volumeSliderX { get; set; }
         public int volumeSliderY { get; set; }
         public string volumeSliderBGColor { get; set; }
         public string volumeSliderFGColor { get; set; }
-
-        // Set position of Current track label
         public int currentTrackLabelX { get; set; }
         public int currentTrackLabelY { get; set; }
         public string currentTrackLabelBGColor { get; set; }
         public string currentTrackLabelFGColor { get; set; }
-
-        // Logo settings
         public string logoImage { get; set; }
         public int logoX { get; set; }
         public int logoY { get; set; }
         public int logoWidth { get; set; }
         public int logoHeight { get; set; }
-
-        // Settings button
         public string settingsButtonImage { get; set; }
         public string settingsButtonHoverImage { get; set; }
         public string settingsButtonPressedImage { get; set; }
@@ -54,8 +39,6 @@
         public int settingsButtonY { get; set; }
         public int settingsButtonWidth { get; set; }
         public int settingsButtonHeight { get; set; }
-
-        // Play button
         public string playButtonImage { get; set; }
         public string playButtonHoverImage { get; set; }
         public string playButtonPressedImage { get; set; }
@@ -63,8 +46,6 @@
         public int playButtonY { get; set; }
         public int playButtonWidth { get; set; }
         public int playButtonHeight { get; set; }
-
-        // Stop button
         public string stopButtonImage { get; set; }
         public string stopButtonHoverImage { get; set; }
         public string stopButtonPressedImage { get; set; }
@@ -72,8 +53,6 @@
         public int stopButtonY { get; set; }
         public int stopButtonWidth { get; set; }
         public int stopButtonHeight { get; set; }
-
-        // Open Button
         public string openButtonImage { get; set; }
         public string openButtonHoverImage { get; set; }
         public string openButtonPressedImage { get; set; }
@@ -81,8 +60,6 @@
         public int openButtonY { get; set; }
         public int openButtonWidth { get; set; }
         public int openButtonHeight { get; set; }
-
-        // Close Button
         public string closeButtonImage { get; set; }
         public string closeButtonHoverImage { get; set; }
         public string closeButtonPressedImage { get; set; }
@@ -90,8 +67,6 @@
         public int closeButtonY { get; set; }
         public int closeButtonWidth { get; set; }
         public int closeButtonHeight { get; set; }
-
-        // previous button
         public string previousButtonImage { get; set; }
         public string previousButtonHoverImage { get; set; }
         public string previousButtonPressedImage { get; set; }
@@ -99,8 +74,6 @@
         public int previousButtonY { get; set; }
         public int previousButtonWidth { get; set; }
         public int previousButtonHeight { get; set; }
-
-        // next button
         public string nextButtonImage { get; set; }
         public string nextButtonHoverImage { get; set; }
         public string nextButtonPressedImage { get; set; }
@@ -108,11 +81,7 @@
         public int nextButtonY { get; set; }
         public int nextButtonWidth { get; set; }
         public int nextButtonHeight { get; set; }
-
-        // ONLY SETTINGS WINDOW COMPONENTS
         public string bgSettingsImage { get; set; }
-
-        // ok button
         public string okButtonImage { get; set; }
         public string okButtonHoverImage { get; set; }
         public string okButtonPressedImage { get; set; }
@@ -120,8 +89,6 @@
         public int okButtonY { get; set; }
         public int okButtonWidth { get; set; }
         public int okButtonHeight { get; set; }
-
-        // cancel button
         public string cancelButtonImage { get; set; }
         public string cancelButtonHoverImage { get; set; }
         public string cancelButtonPressedImage { get; set; }
@@ -129,26 +96,17 @@
         public int cancelButtonY { get; set; }
         public int cancelButtonWidth { get; set; }
         public int cancelButtonHeight { get; set; }
-
-        // Select skin label
         public string selectSkinLabel { get; set; }
         public string selectSkinLabelBGColor { get; set; }
         public string selectSkinLabelFGColor { get; set; }
-
         public int selectSkinLabelX { get; set; }
         public int selectSkinLabelY { get; set; }
-
         public int skinComboBoxX { get; set; }
-
         public int skinComboBoxY { get; set; }
-
         public int skinComboBoxHeight { get; set; }
-
         public int skinComboBoxWidth { get; set; }
-
         public string skinComboBoxBGColor { get; set; }
         public string skinComboBoxFGColor { get; set; }
-
     }
 }
 
--- SIDStreamer/SkinSettings.cs
+++ SIDStreamer/SkinSettings.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace SIDStream
 {
+    // Class used to serialize and deserialize skin settings from JSON
     internal class SkinSettings
     {
         public string skinName { get; set; }
